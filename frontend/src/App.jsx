040fda779b3758646bce906f6288caead96258ca
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-07-13 19:24:14 +0530

    headder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import BookDemo from "./maincomp/BookDemo";
 import Footer from "./maincomp/Footer";
+import Nave from "./maincomp/Nave";
 
 function App() {
   return (
     <>
+      <Nave />
       <BookDemo />
       <Footer />
     </>
